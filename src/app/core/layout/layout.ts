8e59c872cbf18f1cef170d1be292adfeabbe4d36
Ghizlane <ghizlanebelkhanfar@gmail.com>
2026-05-18 18:30:35 +0100

    Add realtime caisse session status badge

--- src/app/core/layout/layout.ts
+++ src/app/core/layout/layout.ts
@@ -1,8 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Router, RouterLink, RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { AuthService } from '../../shared/models/auth.service';
 import { Toast } from '../../shared/toast/toast';
+import { CaisseSessionService } from '../../features/caisse-session/caisse-session.service';
+import { interval, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-layout',
@@ -11,28 +13,60 @@ import { Toast } from '../../shared/toast/toast';
   templateUrl: './layout.html',
   styleUrl: './layout.css'
 })
-export class Layout {
+export class Layout implements OnInit, OnDestroy {
+
+   isDarkMode = false;
+  hasOpenCaisseSession = false;
+  sessionCheckSubscription?: Subscription;
 
   constructor(private router: Router,
-      public authService: AuthService
+      public authService: AuthService,
+        private caisseSessionService: CaisseSessionService
+
 
   ) {}
 
+  ngOnInit(): void {
 
-  isDarkMode = false;
+  this.checkCaisseSession();
 
-toggleDarkMode(): void {
-  this.isDarkMode = !this.isDarkMode;
-
-  if (this.isDarkMode) {
-    document.body.classList.add('dark-mode');
-  } else {
-    document.body.classList.remove('dark-mode');
-  }
+  this.sessionCheckSubscription = interval(3000)
+    .subscribe(() => {
+      this.checkCaisseSession();
+    });
 }
+
+ngOnDestroy(): void {
+  this.sessionCheckSubscription?.unsubscribe();
+}
+
+
+  checkCaisseSession(): void {
+    this.caisseSessionService.getOpenSession()
+      .subscribe({
+        next: (session: any) => {
+          this.hasOpenCaisseSession = !!session;
+        },
+        error: () => {
+          this.hasOpenCaisseSession = false;
+        }
+      });
+  }
+
+  
+
+  toggleDarkMode(): void {
+    this.isDarkMode = !this.isDarkMode;
+
+    if (this.isDarkMode) {
+      document.body.classList.add('dark-mode');
+    } else {
+      document.body.classList.remove('dark-mode');
+    }
+  }
 
   logout(): void {
-  this.authService.logout();
-  this.router.navigate(['/login']);
-}
+    this.authService.logout();
+    this.router.navigate(['/login']);
+  }
 }
